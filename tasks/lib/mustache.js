5d186fe4362b6748e3163df42b48b2ebc9659a62
Pablo Lacerda de Miranda <pablolm@yahoo-inc.com>
2013-12-05 11:41:41 -0800

    moves the compile logic to a separated library

---
+++ tasks/lib/mustache.js
@@ -0,0 +1,63 @@
+/* node: true */
+
+'use strict';
+
+var templateCount = 0;
+var extension = 'mustache';
+
+exports.init = function (grunt) {
+    return {
+        count: function () {
+            return "" + templateCount;
+        },
+
+        compile: function (templateDirs, options) {
+
+            options = options || {};
+            extension = options.extension || extension;
+
+            var prefix = options.prefix || '(',
+                postfix = options.postfix || '',
+                templates = '';
+
+            if( prefix === '(') {
+              postfix = ')' + postfix;
+            }
+
+            var isTemplate = function (abspath) {
+                return abspath.split('.').pop() === extension;
+            };
+
+            var templateName = function (filename) {
+                if (options.keepTemplateSuffix) {
+                    return filename;
+                }
+                var file_extension = '.' + extension;
+                return filename.split(file_extension)[0];
+            };
+
+            var templateHandler = function(abspath, rootdir, subdir, filename) {
+                if(! isTemplate(abspath) ) {
+                    return;
+                }
+                templateCount += 1;
+                templates += '"' + templateName(filename) + '"' + ' : \'' + grunt.file.read(abspath) + '\','+"\n";
+                
+                if (options.verbose) {
+                    grunt.log.writeln('Reading file: '.white + filename.yellow);
+                }   
+            };
+
+            templateCount = 0;
+            templateDirs.forEach( function(file) {
+                grunt.file.recurse(file, templateHandler);
+            });
+
+            templates = '{\n    ' + templates.replace( /\r|\n|\t|\s\s/g, '') + "\n";
+            templates += '    "done": "true"\n  }';
+
+            return prefix + templates + postfix;
+
+        }
+    };
+}
--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
 'use strict';
 
 var grunt = require('grunt');
+var path = require('path');
+var mustache = require('../tasks/lib/mustache').init(grunt);
 
 exports.nodeunit = {
   check_simple_template: function(test) {
@@ -15,7 +17,6 @@ exports.nodeunit = {
 
   check_template_with_prefix: function(test) {
     var actual = grunt.file.read('tmp/templates_with_prefix.js'),
-        ctx = null,
         prefix = "some_prefix",
         some_prefix;
 
@@ -28,7 +29,6 @@ exports.nodeunit = {
 
   check_template_with_prefix_and_postfix: function (test) {
     var actual = grunt.file.read('tmp/templates_with_prefix_and_postfix.js'),
-        ctx = null,
         prefix = "some_prefix",
         some_prefix;
 
@@ -41,7 +41,6 @@ exports.nodeunit = {
 
   "check template with a different extension than mustache will be parsed": function (test) {
     var actual = grunt.file.read('tmp/templates_with_different_extension.js'),
-        ctx = null,
         prefix = "some_prefix",
         some_prefix;
 
